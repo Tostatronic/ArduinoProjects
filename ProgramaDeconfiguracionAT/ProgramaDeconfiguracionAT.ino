37220f5b8bdd8887d6fa6f92cfbadce7d9f2a334
VideoTutorialesTostatronic <team@tostratonic.com>
2016-08-04 13:30:22 -0500

    Video tutorial #3

---
+++ ProgramaDeconfiguracionAT/ProgramaDeconfiguracionAT.ino
@@ -0,0 +1,14 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial1.begin(38400);
+  Serial.println("Dispositivo listo para recibir comando AT");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(Serial.available())
+    Serial1.write(Serial.read());
+  if(Serial1.available())
+    Serial.write(Serial1.read());
+}
